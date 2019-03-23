3c12dfa29547d7cf5cb098a1bb4497f6b5a9516d
abhinuv-dev <abhinuvpitale@gmail.com>
2019-03-23 00:32:17 -0400

    added travel

---
+++ other/travel/india/state_data.js
@@ -0,0 +1,78 @@
+var states = {
+	AP: {
+		name: "Andhra Pradesh",
+		visited: "Unknown",
+		description: "Tirupati"
+	},
+	GA: {
+		name: "Goa",
+    visited: "Unknown",
+		description: "Fam Trip 2007 <br> Friends 2015"
+	},
+	GJ: {
+		name: "Gujarat",
+    visited: "Unknown",
+		description: "Visted Dad in Baroda, Ahmedabad 2010"
+	},
+	HP: {
+		name: "Himachal Pradesh",
+    visited: "Unknown",
+    description: "Trek 2007"
+	},
+	KA: {
+		name: "Karnataka",
+		lived: "2016",
+		description: "Bangalore 2016-17, Mysore, Ooty, Wayanad"
+	},
+  KL: {
+    name: "Kerela",
+    visited: "2017",
+    description: "Trip"
+  },
+	MH: {
+		name: "Maharashtra",
+		lived: "till 2016",
+    description: "Home"
+	},
+	MP: {
+		name: "Madhya Pradesh",
+		visited: "",
+    description: "Trip"
+	},
+	PB: {
+		name: "Punjab",
+		visited: "2015",
+    description : "IV 2015 LOL"
+	},
+  PY: {
+    name: "Puducherry",
+    visited: "2016",
+    description: "MBRID Trip 2016"
+  },
+  RJ: {
+    name: "Rajasthan",
+    visited: "2",
+    description: ""
+  },
+	TN: {
+		name: "Tamil Naidu",
+		visited: "2016",
+    description: "Visa Interview in Chennai"
+	},
+  UP: {
+    name: "Uttar Pradesh",
+    visited: "",
+    description: ""
+  },
+  UT:
+  {
+    name: "Uttarakhand",
+    visited: "",
+    description: "trek"
+  },
+  WB: {
+    name: "West Bengal",
+    visited: "2015",
+    description: "IV trip 2015 LOL"
+  }
+}
---
+++ other/travel/us/state_data.js
@@ -0,0 +1,47 @@
+var states = {
+  AZ: {
+    name: "Arizona",
+    visited: "2018-11-01",
+    description: "Grand Canyon Trip @ 2018"
+  },
+  CA: {
+    name: "California",
+    lived: "2018-08-27",
+    description: "Internship at KittyHawk "
+  },
+    DC2: {
+    name: "Washington, District of Columbia",
+    lived: "2018-05-15",
+    description: "Internship at DeepSig"
+  },
+  	MD: {
+		name: "Maryland",
+		visited: "2018-06-30",
+		description: "Visited Shruti Modak in 2018!"
+	},
+  	NC: {
+		name: "North Carolina",
+		visited: "2018-03-09",
+		description: "Visited Agashe @ NCSU and Pooja @ Cary in 2018"
+	},
+  	PA: {
+		name: "Pennsylvania",
+		visited: "2018-07-15",
+    description: "Visited Sramana @ Philly in 2018 "
+	},
+  	TN: {
+		name: "Tennessee",
+		visited: "2018-08-20",
+    description: "Smoky Mountains Awesome Trip 2018"
+	},
+    TX: {
+    name: "Texas",
+  	visited: "2019-02-07",
+    description: "Visited Surabhi @ Austin 2019"
+  },
+	  VA: {
+		name: "Virginia",
+		lived: "2017-08-15",
+    description: "My Hokie Home 2017"
+	}
+}
