9c6d21c5163f1eb7293cb97f0b85840b8710569f
abhinuv-dev <abhinuvpitale@gmail.com>
2019-03-22 23:26:01 -0400

    added blog.js

---
+++ blog.js
@@ -0,0 +1,56 @@
+const blogList = [
+  {
+    title: "Adafruit Blog Feature",
+    date: "Feb 2019",
+    description: "My project got featured on the adafruit blog. The DIY Maker in me is freaking out! :D ",
+    first: "Yes"
+  },
+  {
+    title: "Habit Pi",
+    date: "Feb 2019",
+    description: "HabitPi - Daily Habit Tracker I made using Raspberry Pi. The blog describes the thought behind the idea and its inspiration.",
+    first: "Yes"
+  },
+  {
+    title: "The Startup Internship - DeepSig",
+    date: "Jan 2019",
+    description: "My experience of working at a early phase startup!(6th person on team)",
+    first: ""
+  },
+  {
+    title: "ECE Spring @ Virginia Tech - Course Review",
+    date: "Dec 2018,
+    description: "My review of the courses I took for Spring 2018 at Virginia Tech.",
+    first: ""
+  },
+  {
+    title: "ECE Fall @ Virginia Tech - Course Review",
+    date: "June 2018",
+    description: "My review of the courses I took for Fall 2017 at Virginia Tech. I hope it helps the incoming graduate students decide.",
+    first: ""
+  },
+  {
+    title: "Ping pong using the brain machine interface",
+    date: "April 2018",
+    description: "Video of a cool practical experiment <a href='https://naresh1318.github.io/'>Naresh</a> and I designed for undergrads at the department of the neuroscience.",
+    first: ""
+  },
+  {
+    title: "Working on campus: one grad’s D2 experience",
+    date: "April 2018",
+    description: "My experience of working at a Dining Center at my university.",
+    first: ""
+  },
+  {
+    title: "History of Wearable and Ubiquitous Computing",
+    date: "March 2018",
+    description: "Describing Mark Weiser's view of computing that disappears into the fabri of everyday life.",
+    first: ""
+  },
+  {
+    title: "History and Challenges of Wearable Computing",
+    date: "January 2018",
+    description: "Interesting and Innovative ideas as presented in research papers from the 1990s. The article also discusses Project Aura, Thad Starner(the man who made AR cool) and a side note about a famous mathematician's wearable device made to cheat at gambling.",
+    first: ""
+  }
+]
