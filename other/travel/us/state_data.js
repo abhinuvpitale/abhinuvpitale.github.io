db49b87cb17641e234320753d0519b9becf1adf5
Abhinuv Nitin Pitale <abhinuvpitale@users.noreply.github.com>
2023-07-10 22:09:02 -0700

    Updated to mid-2023; Formatting changes

--- other/travel/us/state_data.js
+++ other/travel/us/state_data.js
@@ -1,25 +1,40 @@
 var states = {
-  AZ: {
-    name: "Arizona",
-    visited: "2018-11-01",
-    description: "Grand Canyon Trip @ 2018"
-  },
-  CA: {
-    name: "California",
-    lived: "2018-08-27",
-    description: "Internship at KittyHawk, Summer 2019 \n Current home since June 2019"
-  },
-    DC2: {
-    name: "Washington, District of Columbia",
-    lived: "2018-05-15",
-    description: "Internship at DeepSig, Summer 2018"
-  },
-  	MD: {
+	AZ: {
+		name: "Arizona",
+		visited: "2018-11-01",
+		description: "Grand Canyon Trip @ 2018"
+	},
+	CA: {
+		name: "California",
+		lived: "2018-08-27",
+		description: "Internship at KittyHawk, Summer 2019 \n Current home since June 2019"
+	},
+	CO: {
+		name: "Colorado",
+		visited: "2023",
+		description: "Rocky, Great Sand Dunes, all the touristy things"
+	},
+	DC2: {
+		name: "Washington, District of Columbia",
+		lived: "2018-05-15",
+		description: "Internship at DeepSig, Summer 2018"
+	},
+	ID: {
+		name: "Idaho",
+		visited: "2021",
+		description: "Helena National Forest, while moving to Seattle"
+	},
+	MD: {
 		name: "Maryland",
 		visited: "2018-06-30",
 		description: "Visited Shruti Modak in 2018!"
 	},
-  	NC: {
+	MT: {
+		name: "Montana",
+		visited: "2021",
+		description: "Helena National Forest, while moving to Seattle"
+	},
+	NC: {
 		name: "North Carolina",
 		visited: "2018-03-09",
 		description: "Visited Agashe @ NCSU and Pooja @ Cary in 2018"
@@ -29,44 +44,59 @@ var states = {
 		visited: "2019-06-11",
 		description: "Family trip 2019!"
 	},
+	NV: {
+		name: "Nevada",
+		visited: "2021",
+		description: "Roadtrip via Vegas, while moving to Seattle"
+	},
 	NY: {
 		name: "New York",
 		visited: "2019-06-12",
 		description: "New York trip with family 2019!"
 	},
 	PA: {
-	  name: "Pennsylvania",
-	  visited: "2018-07-15",
- 	  description: "Visited Sramana @ Philly in 2018 \n Family trip 2019!"
-  	},
-  	OR: {
-	  name: "Oregon",
-	  visited: "2020-07-01",
-      description: "Crater Lake while driving to Seattle"
-    },
-  	TN: {
+		name: "Pennsylvania",
+		visited: "2018-07-15",
+ 		description: "Visited Sramana @ Philly in 2018 \n Family trip 2019!"
+	},
+	OR: {
+		name: "Oregon",
+		visited: "2020-07-01",
+			description: "Crater Lake while driving to Seattle"
+	},
+	UT: {
+		name: "Utah",
+		visited: "2021",
+		description: "SLC, while moving to Seattle"
+	},
+	TN: {
 		name: "Tennessee",
 		visited: "2018-08-20",
-    description: "Smoky Mountains Awesome Trip 2018"
+		description: "Smoky Mountains Awesome Trip 2018"
 	},
-    TX: {
-    name: "Texas",
-  	visited: "2019-02-07",
-    description: "Visited Surabhi @ Austin 2019"
-  },
+	TX: {
+		name: "Texas",
+		visited: "2019-02-07",
+		description: "Visited Surabhi @ Austin 2019"
+	},
 	VA: {
 		name: "Virginia",
 		lived: "2017-08-15",
-    description: "My Hokie Home 2017-2019"
+		description: "My Hokie Home 2017-2019"
 	},
-        WA: {
-                name: "Washington",
-                visited: "2019-08-27",
-     description: "Visiting Shruti's home in Seattle"
-        },
+	WA: {
+		name: "Washington",
+		lived: "2022",
+		description: "PNW IS THE BEST"
+	},
 	WV: {
 		name: "West Virginia",
 		visited: "2019-04-22",
 		description: "New River Gorge Bridge"
-	}
+	},
+	WY: {
+		name: "Wyoming",
+		visited: "2021",
+		description: "Grand Teton, YellowStone"
+	},
 };
